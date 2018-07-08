c41abc38c094fd826915ef21069f4d3f042ef4b4
Aman Garg <aman.garg@accoliteindia.com>
2018-07-08 23:43:52 +0530

    Add around advice for timer logging

--- src/main/java/com/accolite/au/aopdemo/aspect/LoggingAspect.java
+++ src/main/java/com/accolite/au/aopdemo/aspect/LoggingAspect.java
@@ -1,6 +1,9 @@
 package com.accolite.au.aopdemo.aspect;
 
+import org.apache.commons.lang3.time.StopWatch;
 import org.apache.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -14,5 +17,15 @@ public class LoggingAspect {
 	@Before("execution(public * getRandomList(..))")
 	public void beforeLoggingAdvice() {
 		log.info("Logging before the method gets called");
+	}
+
+	@Around("execution(public * getRandomList(..))")
+	public void aroundLoggingAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		log.info("***Measuring method invocation time.***");
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		proceedingJoinPoint.proceed();
+		stopWatch.stop();
+		log.info("***Method execution completed. Elapsed time: " + stopWatch.getTime() + " ms.***");
 	}
 }
